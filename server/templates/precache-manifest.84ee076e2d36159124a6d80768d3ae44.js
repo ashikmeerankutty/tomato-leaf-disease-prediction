351e966bb8c68bdd98f1f4500b08ce3daa45952c
Ashik Meerankutty <ashik9591@gmail.com>
2020-06-16 15:27:20 +0530

    server: Moved build files to flask server to lett flask handle static files

---
+++ server/templates/precache-manifest.84ee076e2d36159124a6d80768d3ae44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e21567c1634c28ec465dda11b19c9400",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6c3c479ace1853e81b5",
+    "url": "/static/css/main.bfe54808.chunk.css"
+  },
+  {
+    "revision": "876f024e534487b6d265",
+    "url": "/static/js/2.de75c197.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.de75c197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6c3c479ace1853e81b5",
+    "url": "/static/js/main.ebe319ba.chunk.js"
+  },
+  {
+    "revision": "5b155f0fe2cbd540f653",
+    "url": "/static/js/runtime-main.4bd0d543.js"
+  }
+]);
